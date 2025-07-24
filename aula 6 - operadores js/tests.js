732fd4f7a6c0ed25f748d811e7cbc8d75f60e8f6
pahmela <fcunhapamela@gmail.com>
2025-07-24 15:38:24 -0300

    aula 6 - operadores js

---
+++ aula 6 - operadores js/tests.js
@@ -0,0 +1,19 @@
+console.log(`1. ${3 == "3"}`);
+console.log(`2. ${3 === "3"}`);
+
+let a = [];
+let b = "";
+console.log(`3. ${typeof a}`); // object
+
+let c = a == b;
+console.log(`4. ${c}`); // boolean
+
+console.log(`5. ${5 > 3}`);
+console.log(`6. ${10 == 5}`);
+console.log(`7. ${!(5 < 10)}`);
+console.log(`8. ${6 % 2 == 0 && 6 > 3}`);
+console.log(`9. ${5 + "5"} -> ${typeof (5 + "5")}`);
+
+console.log(`10. ${!5}`); // transforma 5 em boolean (true)
+
+

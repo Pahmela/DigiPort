7755e67013745db0e374966f7e2a896e5c191a68
pahmela <fcunhapamela@gmail.com>
2025-07-28 16:56:46 -0300

    aula 7 - pratica operadores js

--- aula 6 - operadores js/tests.js
+++ aula 6 - operadores js/tests.js
@@ -1,7 +1,7 @@
 console.log(`1. ${3 == "3"}`);
 console.log(`2. ${3 === "3"}`);
 
-let a = [];
+let a = [1, 2];
 let b = "";
 console.log(`3. ${typeof a}`); // object
 
@@ -15,5 +15,7 @@ console.log(`8. ${6 % 2 == 0 && 6 > 3}`);
 console.log(`9. ${5 + "5"} -> ${typeof (5 + "5")}`);
 
 console.log(`10. ${!5}`); // transforma 5 em boolean (true)
+
+console.log([2]== "2")
 
 
--- aula 7 - pratica js 2/incremento.js
+++
@@ -1,5 +0,0 @@
-let x = 5;
-console.log(x, x++, x) // Primeiro imprime valor de x e depois incrementa
-
-let y = 5;
-console.log(y, ++y, y) // Primeiro incrementa e depois imprime valor de y
